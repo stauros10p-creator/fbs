24d667089d2cc9a6cc77c360aea4809f3edc0fcd
stauros10p-creator <stauros10p@gmail.com>
2026-06-23 14:10:26 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Layout } from '@/components/layout/Layout'
 import { DataLoader } from '@/components/layout/DataLoader'
-import { AuthGuard } from '@/components/layout/AuthGuard'
 import { DashboardPage } from '@/pages/DashboardPage'
 import { TeamPage } from '@/pages/TeamPage'
+import { EmployeeListPage } from '@/pages/EmployeeListPage'
+import { TopPerformersPage } from '@/pages/TopPerformersPage'
+import { RoleRankingPage } from '@/pages/RoleRankingPage'
+import { ImpactScorePage } from '@/pages/ImpactScorePage'
+import { ProductivityHeatmapPage } from '@/pages/ProductivityHeatmapPage'
 import { SchedulePage } from '@/pages/SchedulePage'
 import { ForecastPage } from '@/pages/ForecastPage'
-import { ForecastHubPage } from '@/pages/ForecastHubPage'
 import { OpsSnapshotPage } from '@/pages/OpsSnapshotPage'
-import { OpsOtdPage } from '@/pages/OpsOtdPage'
-import { OpsThroughputPage } from '@/pages/OpsThroughputPage'
-import { OpsDueDatePage } from '@/pages/OpsDueDatePage'
-import { OpsInboundPage } from '@/pages/OpsInboundPage'
-import { OpsAfixeisPage } from '@/pages/OpsAfixeisPage'
-import { OpsEpistrofesPage } from '@/pages/OpsEpistrofesPage'
-import { OpsLivePage } from '@/pages/OpsLivePage'
 import { CopilotPage } from '@/pages/CopilotPage'
 import { StaffPlanPage } from '@/pages/StaffPlanPage'
 import { HourlyForecastPage } from '@/pages/HourlyForecastPage'
@@ -22,32 +18,26 @@ import { HourlyForecastPage } from '@/pages/HourlyForecastPage'
 export default function App() {
   return (
     <BrowserRouter>
-      <AuthGuard>
-        <DataLoader>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Navigate to="/dashboard" replace />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/team" element={<TeamPage />} />
-              <Route path="/schedule" element={<SchedulePage />} />
-              <Route path="/forecast" element={<ForecastHubPage />} />
-              <Route path="/forecast/staff" element={<ForecastPage />} />
-              <Route path="/ops" element={<OpsSnapshotPage />} />
-              <Route path="/ops/otd" element={<OpsOtdPage />} />
-              <Route path="/ops/throughput" element={<OpsThroughputPage />} />
-              <Route path="/ops/duedate" element={<OpsDueDatePage />} />
-              <Route path="/ops/inbound" element={<OpsInboundPage />} />
-              <Route path="/ops/afixeis" element={<OpsAfixeisPage />} />
-              <Route path="/ops/epistrofes" element={<OpsEpistrofesPage />} />
-              <Route path="/ops/live" element={<OpsLivePage />} />
-              <Route path="/copilot" element={<CopilotPage />} />
-              <Route path="/staff-plan" element={<StaffPlanPage />} />
-              <Route path="/forecast/hourly" element={<HourlyForecastPage />} />
-              <Route path="/hourly-forecast" element={<HourlyForecastPage />} />
-            </Routes>
-          </Layout>
-        </DataLoader>
-      </AuthGuard>
+      <DataLoader>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard"               element={<DashboardPage />} />
+            <Route path="/team"                    element={<TeamPage />} />
+            <Route path="/team/employees"          element={<EmployeeListPage />} />
+            <Route path="/team/top-performers"     element={<TopPerformersPage />} />
+            <Route path="/team/ranking"            element={<RoleRankingPage />} />
+            <Route path="/team/impact"             element={<ImpactScorePage />} />
+            <Route path="/team/heatmap"            element={<ProductivityHeatmapPage />} />
+            <Route path="/schedule"                element={<SchedulePage />} />
+            <Route path="/forecast"                element={<ForecastPage />} />
+            <Route path="/ops"                     element={<OpsSnapshotPage />} />
+            <Route path="/copilot"                 element={<CopilotPage />} />
+            <Route path="/staff-plan"              element={<StaffPlanPage />} />
+            <Route path="/hourly-forecast"         element={<HourlyForecastPage />} />
+          </Routes>
+        </Layout>
+      </DataLoader>
     </BrowserRouter>
   )
 }
